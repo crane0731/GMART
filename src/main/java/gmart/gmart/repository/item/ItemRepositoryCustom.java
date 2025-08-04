8d95ab5f08bb666727b4f11e1028519298f0a214
crane0731 <dlwnsgkr8318@gmail.com>
2025-08-05 00:58:08 +0900

    로그인한 회원이 관심건담에 등록한 건담이 포함되는 상품 리스트 조회 api 개발 완료

--- src/main/java/gmart/gmart/repository/item/ItemRepositoryCustom.java
+++ src/main/java/gmart/gmart/repository/item/ItemRepositoryCustom.java
@@ -1,11 +1,11 @@
 package gmart.gmart.repository.item;
 
-import gmart.gmart.domain.Item;
-import gmart.gmart.domain.Member;
-import gmart.gmart.domain.Store;
+import gmart.gmart.domain.*;
 import gmart.gmart.dto.item.SearchItemCondDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 /**
  * 상품 레파지토리 커스텀
@@ -15,5 +15,6 @@ public interface ItemRepositoryCustom {
     Page<Item> findAllByCond(SearchItemCondDto cond, Pageable pageable);
     Page<Item> findAllByCondAndMember(Member member, SearchItemCondDto cond, Pageable pageable);
     Page<Item> findAllByCondAndStore(Store store, SearchItemCondDto cond, Pageable pageable);
+    Page<Item> findAllByGundams(List<Gundam> gundams, Pageable pageable);
 
 }
